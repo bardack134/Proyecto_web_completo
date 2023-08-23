3094543edcae8c9289d1b4352516eec89ac3f373
bardack134 <142977989+bardack134@users.noreply.github.com>
2023-08-23 17:50:40 +0900

    Add files via upload

---
+++ ProyectoWebApp/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from ProyectoWebApp import views
+
+from django.conf import settings
+
+from django.conf.urls.static import static
+
+urlpatterns = [
+        
+    path('', views.home, name='Home'),
+    
+
+   
+    
+
+
+    
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
